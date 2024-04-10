5ec107758a2dc55bb5856e85dfb405d00482ced4
Titus Owuor <titusowuor30@gmail.com>
2024-04-10 09:17:14 +0300

    merge

--- datapull/utils.py
+++ datapull/utils.py
@@ -8,8 +8,7 @@ class PMTCTDataClient:
 
     def pull_data(self):
         # Make the GET request with basic authentication
-        response = requests.get(
-            self.request_url, params=self.params, auth=self.credentials)
+        response = requests.get(self.request_url, params=self.params, auth=self.credentials)
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Process the response data (in JSON format)
