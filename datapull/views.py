6c51ef08e3b9174302225c681bd0c36ec47caaef
Titus Owuor <titusowuor30@gmail.com>
2024-04-10 13:20:22 +0300

    merge

--- datapull/views.py
+++ datapull/views.py
@@ -60,8 +60,19 @@ def sync_data(request):
     sets = middleware_settings.objects.all().first()
     return Response({"id": sets.id, "synctdata": str(sets.syncdata).lower(), "client_url": sets.client_url})
 
+def classify(diff_anc):
+    anc_status = ""
+    if(diff_anc< 0):
+        anc_status = "critical"
+    elif(diff_anc>=0):
+        anc_status = 'stable'
+    else:
+        anc_status='normal'
+    return anc_status
+
 def dataAnalytics(data):
     data_rows = pd.DataFrame(data['rows'])
+    data_rows[3] = data_rows[3].astype(int)
     ana_lytics = {"data": []}
     try:
         if(len(data_rows)>0):
@@ -74,62 +85,123 @@ def dataAnalytics(data):
                 org_data = {}
                 moh_711_new = data_rows[(data_rows[0] == 'f9vesk5d4IY') & (data_rows[1] == org)]
                 moh_731_HV02_01 = data_rows[(data_rows[0] == 'uSxBUWnagGg') & (data_rows[1] == org)]
-                moh_731_HV02_03 = data_rows[data_rows[0] == 'qSgLzXh46n9']
-                moh_731_HV02_04 = data_rows[data_rows[0] == 'ETX9cUWF43c']
-                moh_731_HV02_05 = data_rows[data_rows[0] == 'mQz4DhBSv9V']
-                moh_731_HV02_06 = data_rows[data_rows[0] == 'LQpQQP3KnU1']
-                moh_731_HV02_10 = data_rows[data_rows[0] == 'oZc8MNc0nLZ']
-                moh_731_HV02_11 = data_rows[data_rows[0] == 'nwXS5vxrrr7']
-                moh_731_HV02_12 = data_rows[data_rows[0] == 'hn3aChn4sVx']
-                moh_731_HV02_13 = data_rows[data_rows[0] == 'AfHArvGun12']
-                moh_731_HV02_14 = data_rows[data_rows[0] == 'hHLR1HP8xzI']
-                moh_731_HV02_16 = data_rows[data_rows[0] == 'lJpaBye9B0H']
-                moh_731_HV02_17 = data_rows[data_rows[0] == 'WNFWVHMqPv9']
-                moh_731_HV02_18 = data_rows[data_rows[0] == 'ckPCoAwmWmT']
-                moh_731_HV02_19 = data_rows[data_rows[0] == 'vkOYqEesPAi']
-                moh_731_HV02_21 = data_rows[data_rows[0] == 'UMyB7dSIdz1']
-                moh_731_HV02_39 = data_rows[data_rows[0] == 'HAumxpKBaoK']
-                moh_731_HV02_40 = data_rows[data_rows[0] == 'Jn6ATTfXp02']
-                moh_731_HV02_41 = data_rows[data_rows[0] == 'RY1js5pK2Ep']
+                moh_731_HV02_03 = data_rows[(data_rows[0] == 'qSgLzXh46n9') & (data_rows[1] == org)]
+                moh_731_HV02_04 = data_rows[(data_rows[0] == 'ETX9cUWF43c') & (data_rows[1] == org)]
+                moh_731_HV02_05 = data_rows[(data_rows[0] == 'mQz4DhBSv9V') & (data_rows[1] == org)]
+                moh_731_HV02_06 = data_rows[(data_rows[0] == 'LQpQQP3KnU1') & (data_rows[1] == org)]
+                moh_731_HV02_10 = data_rows[(data_rows[0] == 'oZc8MNc0nLZ') & (data_rows[1] == org)]
+                moh_731_HV02_11 = data_rows[(data_rows[0] == 'nwXS5vxrrr7') & (data_rows[1] == org)]
+                moh_731_HV02_12 = data_rows[(data_rows[0] == 'hn3aChn4sVx') & (data_rows[1] == org)]
+                moh_731_HV02_13 = data_rows[(data_rows[0] == 'AfHArvGun12') & (data_rows[1] == org)]
+                moh_731_HV02_14 = data_rows[(data_rows[0] == 'hHLR1HP8xzI') & (data_rows[1] == org)]
+                moh_731_HV02_16 = data_rows[(data_rows[0] == 'lJpaBye9B0H') & (data_rows[1] == org)]
+                moh_731_HV02_17 = data_rows[(data_rows[0] == 'WNFWVHMqPv9') & (data_rows[1] == org)]
+                moh_731_HV02_18 = data_rows[(data_rows[0] == 'ckPCoAwmWmT') & (data_rows[1] == org)]
+                moh_731_HV02_19 = data_rows[(data_rows[0] == 'vkOYqEesPAi') & (data_rows[1] == org)]
+                moh_731_HV02_21 = data_rows[(data_rows[0] == 'UMyB7dSIdz1') & (data_rows[1] == org)]
+                moh_731_HV02_39 = data_rows[(data_rows[0] == 'HAumxpKBaoK') & (data_rows[1] == org)]
+                moh_731_HV02_40 = data_rows[(data_rows[0] == 'Jn6ATTfXp02') & (data_rows[1] == org)]
+                moh_731_HV02_41 = data_rows[(data_rows[0] == 'RY1js5pK2Ep') & (data_rows[1] == org)]
 
                 org_name = data['metaData']['items'][org]['name']
 
+                moh_711_new_sum = moh_731_HV02_01_sum = moh_731_HV02_03_sum = moh_731_HV02_04_sum = moh_731_HV02_05_sum = moh_731_HV02_06_sum = moh_731_HV02_10_sum = moh_731_HV02_11_sum = moh_731_HV02_12_sum = moh_731_HV02_13_sum = moh_731_HV02_14_sum = moh_731_HV02_16_sum = moh_731_HV02_17_sum = moh_731_HV02_18_sum = moh_731_HV02_19_sum = moh_731_HV02_21_sum = moh_731_HV02_39_sum = moh_731_HV02_40_sum = moh_731_HV02_41_sum = 0
+
                 if(len(moh_711_new)> 0):
-                    moh_711_new[3] = moh_711_new[3].astype(int)
                     moh_711_new_sum = moh_711_new[3].sum()
+
                 if(len(moh_731_HV02_01)> 0):
-                    moh_731_HV02_01[3] = moh_731_HV02_01[3].astype(int)
                     moh_731_HV02_01_sum = moh_731_HV02_01[3].sum()
+
                 if(len(moh_731_HV02_03)> 0):
-                    moh_731_HV02_03[3] = moh_731_HV02_03[3].astype(int)
                     moh_731_HV02_03_sum = moh_731_HV02_03[3].sum()
+
                 if(len(moh_731_HV02_04)> 0):
-                    moh_731_HV02_04[3] = moh_731_HV02_04[3].astype(int)
                     moh_731_HV02_04_sum = moh_731_HV02_04[3].sum()
 
+                if(len(moh_731_HV02_05)> 0):
+                    moh_731_HV02_05_sum = moh_731_HV02_05[3].sum()
+
+                if(len(moh_731_HV02_06)> 0):
+                    moh_731_HV02_06_sum = moh_731_HV02_06[3].sum()
+
+                if(len(moh_731_HV02_10)> 0):
+                    moh_731_HV02_10_sum = moh_731_HV02_10[3].sum()
+
+                if(len(moh_731_HV02_11)> 0):
+                    moh_731_HV02_11_sum = moh_731_HV02_11[3].sum()
+
+                if(len(moh_731_HV02_12)> 0):
+                    moh_731_HV02_12_sum = moh_731_HV02_12[3].sum()
+
+                if(len(moh_731_HV02_13)> 0):
+                    moh_731_HV02_13_sum = moh_731_HV02_13[3].sum()
+
+                if(len(moh_731_HV02_14)> 0):
+                    moh_731_HV02_14_sum = moh_731_HV02_14[3].sum()
+
+                if(len(moh_731_HV02_16)> 0):
+                    moh_731_HV02_16_sum = moh_731_HV02_16[3].sum()
+
+                if(len(moh_731_HV02_17)> 0):
+                    moh_731_HV02_17_sum = moh_731_HV02_17[3].sum()
+
+                if(len(moh_731_HV02_18)> 0):
+                    moh_731_HV02_18_sum = moh_731_HV02_18[3].sum()
+
+                if(len(moh_731_HV02_19)> 0):
+                    moh_731_HV02_19_sum = moh_731_HV02_19[3].sum()
+
+                if(len(moh_731_HV02_21)> 0):
+                    moh_731_HV02_21_sum = moh_731_HV02_21[3].sum()
+
+                if(len(moh_731_HV02_39)> 0):
+                    moh_731_HV02_39_sum = moh_731_HV02_39[3].sum()
+
+                if(len(moh_731_HV02_40)> 0):
+                    moh_731_HV02_40_sum = moh_731_HV02_40[3].sum()
+
+                if(len(moh_731_HV02_41)> 0):
+                    moh_731_HV02_41_sum = moh_731_HV02_41[3].sum()
 
                 print(f"orgname: {org_name} moh_711_new_sum: {moh_711_new_sum}")
-                diff_anc = moh_731_HV02_01[3].sum()-moh_711_new[3].sum()
+                diff_anc = moh_731_HV02_01_sum-moh_711_new_sum
+                missed_opp = moh_731_HV02_01_sum - (moh_731_HV02_03_sum + moh_731_HV02_04_sum + moh_731_HV02_05_sum + moh_731_HV02_06_sum)
 
-                if(diff_anc< 0):
-                    anc_status = "critical"
-                elif(diff_anc>0):
-                    anc_status = 'stable'
+
+                anc_status = classify(diff_anc)
+                # breakpoint()
+                missed_opp_status = classify(diff_anc=-missed_opp)
 
                 ana_lytics['data'].append({
                     "ou_name": org_name,
                     "moh_711_new": moh_711_new_sum,
                     "moh_731_HV02_01": moh_731_HV02_01_sum,
                     "diff_anc": diff_anc,
-                    "anc_status": anc_status
-
+                    "anc_status": anc_status,
+                    "moh_731_HV02_03": moh_731_HV02_03_sum,
+                    "moh_731_HV02_04": moh_731_HV02_04_sum,
+                    "moh_731_HV02_05": moh_731_HV02_05_sum,
+                    "moh_731_HV02_06": moh_731_HV02_06_sum,
+                    "moh_731_HV02_10": moh_731_HV02_10_sum,
+                    "moh_731_HV02_11": moh_731_HV02_11_sum,
+                    "moh_731_HV02_12": moh_731_HV02_12_sum,
+                    "moh_731_HV02_13": moh_731_HV02_13_sum,
+                    "moh_731_HV02_14": moh_731_HV02_14_sum,
+                    "moh_731_HV02_16": moh_731_HV02_16_sum,
+                    "moh_731_HV02_17": moh_731_HV02_17_sum,
+                    "moh_731_HV02_18": moh_731_HV02_18_sum,
+                    "moh_731_HV02_19": moh_731_HV02_19_sum,
+                    "moh_731_HV02_21": moh_731_HV02_21_sum,
+                    "moh_731_HV02_39": moh_731_HV02_39_sum,
+                    "moh_731_HV02_40": moh_731_HV02_40_sum,
+                    "moh_731_HV02_41": moh_731_HV02_41_sum,
+                    "missed_opp": missed_opp,
+                    "missed_opp_status": missed_opp_status,
                 })
     except Exception as e:
         print(f" Error: {e}")
-
-        ana_lytics['data'].append({})
-
-            # pass
+        ana_lytics['error'].append({"message": f"{e}"})
     return ana_lytics
 
 class DataClientViewSet(viewsets.ViewSet):
